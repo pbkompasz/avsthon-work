be3ab3cb4b634673be13d9e2152517a6ef109974
Peter <kompaszbence@gmail.com>
2024-10-30 00:53:39 +0100

    refactor: cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,32 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import Contract from "./components/Contract.tsx";
-import Settings from "./components/Settings.tsx";
-import Layout from "./components/Layout.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  hardhat,
+} from "wagmi/chains";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { WagmiProvider } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./components/App.tsx";
+import Contract from "./components/contracts/Contract.tsx";
+import Settings from "./components/Settings.tsx";
+import Layout from "./components/Layout.tsx";
+import Marketplace from "./components/Marketplace.tsx";
+import "../node_modules/@rainbow-me/rainbowkit/dist/index.css";
+import "./index.css";
 
 const queryClient = new QueryClient();
-
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-
-import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
-
-import '../node_modules/@rainbow-me/rainbowkit/dist/index.css';
 
 const config = getDefaultConfig({
   appName: "avs",
   projectId: "1243f16501b3ffe219ec79390bb4c171",
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  chains: [mainnet, polygon, optimism, arbitrum, base, hardhat],
   ssr: false,
 });
 
@@ -34,6 +39,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "marketplace",
+    element: <Marketplace />,
   },
   {
     path: "contracts",
@@ -49,7 +58,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider modalSize="compact" >
+        <RainbowKitProvider modalSize="compact">
           <Layout>
             <RouterProvider router={router} />
           </Layout>
